26d8958f97bdbd92992e127fcc6a46487b1c3bdb
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-03-11 01:04:01 +0600

    comment user done

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  likedPostsId.plus(id);
+  likedPostsId.push(id);
   showPosts(posts);
 };
 
@@ -54,6 +54,8 @@ const createPost = (post) => {
   const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
+  console.log(post)
+
   div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
@@ -119,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
